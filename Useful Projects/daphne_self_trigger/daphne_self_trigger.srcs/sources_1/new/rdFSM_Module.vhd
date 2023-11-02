e55158f52f50aeebb9cda505d20e68533fadb6bb
Daniel Avila <daniel.avila@eia.edu.co>
2023-11-02 14:26:50 -0500

    Added DAPHNE Self Trigger Test Bench
    Simulation still needed to run once.

---
+++ Useful Projects/daphne_self_trigger/daphne_self_trigger.srcs/sources_1/new/rdFSM_Module.vhd
@@ -0,0 +1,164 @@
+----------------------------------------------------------------------------------
+-- Company: Universidad EIA
+-- Engineer: Daniel Ávila Gómez
+-- 
+-- Create Date: 17.05.2023 07:16:08
+-- Design Name: Finite State Machine to read data from a FIFO 
+-- Module Name: rdFSM_Module - rdFsm_arch
+-- Project Name: DAPHNE V1 - MEMORY MODULE
+-- Target Devices: XC7A200T-1SBG484C
+-- Tool Versions: 2022.2
+-- Description: 
+-- Creates a FSM to read data from a FIFO, important to ensure no read errors occur
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- This is a Mealy Type FSM, where output depends in both current state and inputs
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity rdFSM_Module is
+    Port ( 
+        -- Module Inputs
+    -----------------------------------------------------------------------------------------------------------------------------------------------------------
+        clk_fsm         : in std_logic;
+        rst             : in std_logic;
+        fifo_Empty      : in std_logic;                         -- Empty flag from FIFO stating that another read operation will damage FIFO
+        ext_rd_en       : in std_logic;                         -- External write enable to control FSM
+        fifo_rd_err     : in std_logic;                         -- If FIFO goes into error state FSM should not allow read operations
+    
+        -- Module Outputs
+    -----------------------------------------------------------------------------------------------------------------------------------------------------------
+        rd_en           : out std_logic;                        -- Reading the FIFO is enabled or disabled
+        rd_err          : out std_logic                         -- Read error state was reached
+    );
+end rdFSM_Module;
+
+architecture rdFsm_arch of rdFSM_Module is
+
+-- Declaration of the states type and signal
+type state_type is (reset, idle, read, error);
+signal State, Next_State        : state_type;
+
+-- Aux signals to operate inside the FSM
+signal rd_en_reg                : std_logic := '0'; 
+signal rd_err_reg               : std_logic := '0';
+
+begin
+
+    -- Finite State Machine process
+----------------------------------------------------------------------------------------------------------------------------------------
+    -- Process to change the states of the FSM
+    reg_states: process (rst, clk_fsm)
+    begin
+        if rising_edge(clk_fsm) then
+            if (rst = '1') then
+                State <= reset;
+            else
+                State <= Next_State;
+            end if;
+        end if;
+    end process reg_states;
+
+    -- Process to define when states change
+    mod_states: process (State, ext_rd_en, fifo_rd_err, fifo_Empty)
+    begin
+        Next_State <= State; -- Declare default state for Next_State to avoid latches, default is to stay in current state
+        case (State) is
+            when reset =>
+                Next_State <= idle;
+            when idle =>
+                if (fifo_rd_err = '1') then
+                    Next_State <= error;
+                else
+                    if (ext_rd_en = '1') then
+                        if (fifo_Empty = '0') then
+                            Next_State <= read;
+                        else
+                            Next_State <= error;
+                        end if;
+                    end if;                        
+                end if;
+            when read =>
+                if (fifo_Empty = '1' OR ext_rd_en = '0') then
+                    Next_State <= idle;
+                end if;
+            when error =>
+                if (ext_rd_en = '0' AND fifo_rd_err = '0') then 
+                    Next_State <= idle;
+                end if;
+            when others =>
+                -- This includes reset condition
+                -- Do nothing
+                -- Might add Next_State <= reset;
+        end case;
+    end process mod_states;
+
+    -- Finite State Machine Outputs
+----------------------------------------------------------------------------------------------------------------------------------------
+    -- Combinatorial Process to establish FSM Outputs
+    do_states: process (State, ext_rd_en, fifo_rd_err, fifo_Empty)
+    begin
+        case (State) is
+            when reset =>
+                rd_en_reg <= '0';
+                rd_err_reg <= '0';
+            when idle =>
+                if (fifo_rd_err = '1') then
+                    rd_en_reg <= '0';
+                    rd_err_reg <= '1';
+                else
+                    if (ext_rd_en = '1') then
+                        if (fifo_Empty = '0') then
+                            rd_en_reg <= '1';
+                            rd_err_reg <= '0';
+                        else
+                            rd_en_reg <= '0';
+                            rd_err_reg <= '1';
+                        end if;   
+                    else 
+                        rd_en_reg <= '0';
+                        rd_err_reg <= '0';                     
+                    end if;
+                end if;
+            when read =>
+                if (fifo_Empty = '1' OR ext_rd_en = '0') then
+                    rd_en_reg <= '0';
+                    rd_err_reg <= '0';
+                else
+                    rd_en_reg <= '1';
+                    rd_err_reg <= '0';
+                end if;
+            when error =>
+                rd_en_reg <= '0';
+                rd_err_reg <= '1';
+            when others =>
+                rd_en_reg <= '0';
+                rd_err_reg <= '0'; 
+        end case;    
+    end process do_states;
+    
+    -- Enable reading to FIFO
+----------------------------------------------------------------------------------------------------------------------------------------
+    rd_en                   <= rd_en_reg;
+    
+    -- Read error condition asserted
+    -- FIFO is still safe because of this FSM
+----------------------------------------------------------------------------------------------------------------------------------------
+    rd_err                  <= rd_err_reg;
+
+end rdFsm_arch;
---
+++ Useful Projects/daphne_self_trigger/daphne_self_trigger.srcs/sources_1/new/selfTrigger_Module.vhd
@@ -0,0 +1,312 @@
+------------------------------------------------------------------------------------
+---- Company: Universidad EIA
+---- Engineer: Daniel Avila Gomez
+---- 
+---- Create Date: 15.08.2023 13:33:30
+---- Design Name: Self Trigger Full Module Integration
+---- Module Name: selfTrigger_Module - selfTrig_arch
+---- Project Name: AFE5808A - Nexys Video Full Integration
+---- Target Devices: XC7A200T-1SBG484C
+---- Tool Versions: 2023.1
+---- Description: 
+---- Creates a module that connects the high pass filter and the self trigger module
+---- Dependencies: 
+---- Relies on the 'highPass_FirstOrder.vhd' and 'Self-trigger_VHDL.vhd' files
+---- Revision:
+---- Revision 0.01 - File Created
+---- Additional Comments:
+---- 
+------------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL; 
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity selfTrigger_Module is
+    Port ( 
+        -- Module Inputs
+    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        afe_data            : in std_logic_vector(13 downto 0);     -- Aligned Data to Save from AFE
+        dt_rdy              : in std_logic;                         -- Data Aligned
+        rst                 : in std_logic;                         -- Async Reset
+        clk                 : in std_logic;                         -- AFE Divided Clock Used by the High Pass Filter (62.5 MHz) And the Write Clock of the FIFO 
+        rd_clk              : in std_logic;                         -- Read Clock Used by the FIFO (62.5 MHz) Asynchronous to WR_CLK    
+        sys_clk             : in std_logic;                         -- This is AXI Stream Clock (125 MHz)
+        rd_ctrl             : in std_logic;                         -- Read Control Input ('0' Don't read the FIFO after a save, '1' Read the FIFO as soon as it stops saving)
+        wr_enable_signal    : in std_logic;                         -- Auxiliar External Write Enable for the FIFO
+        rd_enable_signal    : in std_logic;                         -- Auxiliar External Read Enable for the FIFO
+        
+        -- Module Outputs
+    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        filt_out            : out std_logic_vector(13 downto 0);    -- Output of the filter
+        xcorr_out           : out std_logic_vector(47 downto 0);    -- Output of the Self Trigger Correlation Module
+        xcorr_data_out      : out std_logic_vector(13 downto 0);    -- Output of the Self Trigger Correlation Module (Internally connected to the FIFO, 64 Registers Delayed AFE Data)
+        trigger             : out std_logic;                        -- Trigger Output
+        fifo_rd_out         : out std_logic;                        -- Real Read Enable used for the FIFO (Mapped Internally)
+        fifo_wr_out         : out std_logic;                        -- Real Write Enable used for the FIFO (Mapped Internally)
+        fifo_o              : out std_logic_vector(13 downto 0);    -- Output Data of the FIFO    
+        fifo_a_empty        : out std_logic;                        -- Almost Empty Flag of the FIFO
+        fifo_a_full         : out std_logic;                        -- Amost Full Flag of the FIFO (Interpreted as the real Full of the FIFO)
+        fifo_empty          : out std_logic;                        -- Empty Flag of the FIFO
+        fifo_full           : out std_logic;                        -- Full Flag of the FIFO
+        fifo_wr_err         : out std_logic;                        -- Write Error Flag of the FIFO (Internally Generated Not Real One)
+        fifo_rd_err         : out std_logic;                        -- Read Error Flag of the FIFO (Internally Generated Not Real One)
+        axi_data            : out std_logic_vector(7 downto 0);     -- Output Data of the FIFO in AXI Stream Interface
+        axi_valid           : out std_logic;                        -- AXI Stream Valid from the FIFO Read Process
+        axi_ready           : in std_logic;                         -- AXI Stream Ready for the FIFO Read Process
+        axi_last            : out std_logic;                        -- AXI Stream Last from the FIFO Read Process
+        axi_user            : out std_logic                         -- AXI Stream User from the FIFO Read Process (Always '1')
+    );
+end selfTrigger_Module;
+
+architecture Behavioral of selfTrigger_Module is
+
+-- High Pass Module Declaration
+--------------------------------------------------------------------------------------------------------------------------------------------------
+component highPass_FirstOrder 
+    Generic (
+        Data_Size               : integer   := 14;
+        Coefficient_Resolution  : integer   := 17                                   -- One more than decimal desired
+    );
+    Port (
+        -- Module Inputs
+    ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        rst                     : in std_logic;                                     -- Reset for the filter
+        clk                     : in std_logic;                                     -- Clock for the filter
+        x_in                    : in std_logic_vector((Data_Size - 1) downto 0);    -- Input vector from AFEs
+        
+        -- Module Output
+    ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        y_out                   : out std_logic_vector((Data_Size - 1) downto 0)    -- Output vector from Filter
+    );
+end component highPass_FirstOrder;
+
+component self_trigger 
+    Generic (
+        g_INPUT_WIDTH           : natural   := 14;                                      -- Width of the Input Data
+        g_SUM_WIDTH             : natural   := 14;                                      -- Width of the Internal Addition
+        g_MULT_WIDTH            : natural   := 28                                       -- Width of the Internal Multiplication
+    );
+    Port (
+        -- Module Inputs
+    --------------------------------------------------------------------------------------------------------------------------------------
+        clk                     : in  std_logic;                                        -- Clock for the Module
+        rst                     : in std_logic;                                         -- Async Reset
+        i_data                  : in  std_logic_vector(g_INPUT_WIDTH - 1 downto 0);     -- Input Parallel Data from the Acquisition Module
+        data_hpf                : in std_logic_vector(g_INPUT_WIDTH - 1 downto 0);      -- Filtered Data from the High Pass Filter
+        
+        -- Module Outputs
+    --------------------------------------------------------------------------------------------------------------------------------------
+        o_data                  : out std_logic_vector(g_SUM_WIDTH - 1 downto 0);       -- Output Data (n Samples Delayed)
+        o_xcorr                 : out std_logic_vector(47 downto 0);                    -- Output Correlation Value
+        o_trigger               : out std_logic                                         -- Trigger Output
+    );
+end component;
+
+-- FIFO Write & Read Control FSM Module Declaration
+--------------------------------------------------------------------------------------------------------------------------------------------------
+component trigSaveReadFSM 
+    Port ( 
+        -- Module Inputs
+    --------------------------------------------------------------------------------------------------------------------------------------
+        WR_CLK                  : in std_logic;         -- Write Clock for the FIFO
+        RD_CLK                  : in std_logic;         -- Read Clock for the FIFO
+        RST                     : in std_logic;         -- Async Reset
+        TRIG_IN                 : in std_logic;         -- Trigger Input
+        READ_IN                 : in std_logic;         -- External Read of the FIFO
+        FIFO_FULL               : in std_logic;         -- FIFO Full Flag
+        FIFO_EMPTY              : in std_logic;         -- FIFO Empty Flag
+        RD_AFTER                : in std_logic;         -- Read Immediately After a FIFO Write
+        WR_AFTER                : in std_logic;         -- Write Immediately After a FIFO Read
+        
+        -- Module Outputs
+    --------------------------------------------------------------------------------------------------------------------------------------
+        TRIG_OUT                : out std_logic;        -- Output Trigger (Stable Signal For n Samples)
+        READ_OUT                : out std_logic         -- Output Read (Stable Signal For n Samples)
+    );
+end component trigSaveReadFSM;
+
+-- FIFO Memory With AXI Stream Output Module Declaration
+--------------------------------------------------------------------------------------------------------------------------------------------------
+component AXI_FIFO_Adapter
+    Generic (
+        -- Frequency of the Write Clock (MHz)
+        WR_CLK_FREQ         : real          := 100.0;
+        -- Frequency of the Read Clock (MHz)
+        RD_CLK_FREQ         : real          := 100.0;
+        -- FIFO Important Parameters
+        data_width          : integer       := 14;
+        fifoPointersLength  : integer       := 11;
+        AEMPTY_OFF          : bit_vector    := X"0080";                                 -- Almost Empty Offset
+        AFULL_OFF           : bit_vector    := X"0080"                                  -- Almost Full Offset
+    );
+    Port (
+        -- Module Inputs
+    ----------------------------------------------------------------------------------------------------------------------------------
+        d_i                 : in std_logic_vector((data_width - 1) downto 0);           -- Parallel Data Input for the FIFO
+        dt_rdy              : in std_logic;                                             -- Data Ready Flag from Acquisition Module
+        wr_enable           : in std_logic;                                             -- Write Enable Control Signal for the FIFO
+        rd_enable           : in std_logic;                                             -- Read Enable Control Signal for the FIFO
+        wr_clk              : in std_logic;                                             -- Write Clock for the FIFO
+        rd_clk              : in std_logic;                                             -- Read Clock for the FIFO
+        m_axi_clk           : in std_logic;                                             -- AXI Stream Interface Clock
+        rst                 : in std_logic;                                             -- Async Reset
+    
+        -- Module Outputs
+    ----------------------------------------------------------------------------------------------------------------------------------    
+        a_empty             : out std_logic;                                            -- Almost Empty Flag of the FIFO
+        a_full              : out std_logic;                                            -- Almost Full Flag of the FIFO
+        empty               : out std_logic;                                            -- Empty Flag of the FIFO
+        full                : out std_logic;                                            -- Full Flag of the FIFO 
+        wr_err              : out std_logic;                                            -- Write Error Flag of the FIFO
+        rd_err              : out std_logic;                                            -- Read Error Flag of the FIFO
+        wr_count            : out std_logic_vector((fifoPointersLength - 1) downto 0);  -- Write Pointer of the FIFO
+        rd_count            : out std_logic_vector((fifoPointersLength - 1) downto 0);  -- Read Pointer of the FIFO
+        fifo_o              : out std_logic_vector(13 downto 0);                        -- Parallel Data Output of the FIFO
+        m_axi_fifo_tdata    : out std_logic_vector((data_width/2) downto 0);            -- AXI Stream Data from the FIFO
+        m_axi_fifo_tvalid   : out std_logic;                                            -- AXI Stream Valid Control Signal
+        m_axi_fifo_tready   : in std_logic;                                             -- AXI Stream Ready Control Signal
+        m_axi_fifo_tlast    : out std_logic;                                            -- AXI Stream Last Control Signal
+        m_axi_fifo_tuser    : out std_logic                                             -- AXI Stream User Control Signal (Errors in the data)
+    );
+end component AXI_FIFO_Adapter;
+
+-- Internal Connections and Auxiliary Signals
+--------------------------------------------------------------------------------------------------------------------------------------------------
+signal data_aux             : std_logic_vector(13 downto 0);
+signal trigger_aux          : std_logic;
+signal read                 : std_logic;
+signal trigger_in           : std_logic;
+signal fifo_trig            : std_logic;
+signal fifo_rd              : std_logic; 
+signal afe_fifo_a_empty     : std_logic;
+signal afe_fifo_a_full      : std_logic;
+signal afe_fifo_empty       : std_logic;
+signal afe_fifo_full        : std_logic;
+signal afe_fifo_wr_err      : std_logic;
+signal afe_fifo_rd_err      : std_logic;
+signal xcorr_data_out_aux   : std_logic_vector(13 downto 0);
+
+begin
+    
+    -- High Pass First Order Filter Component Instantiation
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    FILTER_COM : highPass_FirstOrder     
+        port map (
+            -- Module Inputs
+        ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            rst             => rst,
+            clk             => clk,
+            x_in            => afe_data,
+            
+            -- Module Outputs
+        --------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+            y_out           => data_aux 
+        );
+    
+    SELF_TRIGGER_TOP_COM : self_trigger
+    port map (
+        clk => clk,
+        rst => rst,
+        i_data => afe_data,
+        data_hpf => data_aux,
+        o_data => xcorr_data_out_aux,
+        o_xcorr => xcorr_out,
+        o_trigger => trigger_aux
+    );
+    
+    -- Trigger signal can be either the external trigger input or the self triggering one
+    trigger_in              <= wr_enable_signal OR trigger_aux; --uncomment for real one, comment to test if write enable and read enable are properly working
+    
+    -- FIFO can be read either manually or automatically depending on the configuration of the RD_AFTER input
+    -- If the FIFO is read manually, it depends on the rd_enable signal
+    
+    -- FIFO Writing and Reading FSM
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    FSM_CTRL_COM : trigSaveReadFSM
+        port map (
+            -- Module Inputs
+        --------------------------------------------------------------------------------------------------------------------------------------
+            WR_CLK          => clk,
+            RD_CLK          => rd_clk,
+            RST             => rst,
+            TRIG_IN         => trigger_in,
+            READ_IN         => rd_enable_signal, 
+            FIFO_FULL       => afe_fifo_a_full,
+            FIFO_EMPTY      => afe_fifo_empty,
+            RD_AFTER        => rd_ctrl, 
+            WR_AFTER        => '0',
+            
+            -- Module Outputs
+        --------------------------------------------------------------------------------------------------------------------------------------
+            TRIG_OUT        => fifo_trig, 
+            READ_OUT        => fifo_rd 
+        );
+    
+    -- FIFO Component Instantiation
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    FIFO_INST_COM : AXI_FIFO_Adapter
+        generic map (
+            -- Frequency of the Write Clock (MHz)
+            WR_CLK_FREQ         => 62.5,
+            -- Frequency of the Read Clock (MHz)
+            RD_CLK_FREQ         => 62.5,
+            -- FIFO Important Parameters
+            AEMPTY_OFF          => X"6",
+            AFULL_OFF           => X"8"
+        )
+        port map (
+            -- Module Inputs
+        ----------------------------------------------------------------------------------------------------------------------------------
+            d_i                 => xcorr_data_out_aux,
+            dt_rdy              => dt_rdy,
+            wr_enable           => fifo_trig,
+            rd_enable           => fifo_rd,
+            wr_clk              => clk,
+            rd_clk              => rd_clk,
+            m_axi_clk           => sys_clk,
+            rst                 => rst,    
+            
+            -- Module Outputs
+        ----------------------------------------------------------------------------------------------------------------------------------    
+            a_empty             => afe_fifo_a_empty,
+            a_full              => afe_fifo_a_full,
+            empty               => afe_fifo_empty,
+            full                => afe_fifo_full,
+            wr_err              => afe_fifo_wr_err,
+            rd_err              => afe_fifo_rd_err,
+            wr_count            => open,
+            rd_count            => open,
+            fifo_o              => fifo_o,
+            m_axi_fifo_tdata    => axi_data,
+            m_axi_fifo_tvalid   => axi_valid,
+            m_axi_fifo_tready   => axi_ready,
+            m_axi_fifo_tlast    => axi_last,
+            m_axi_fifo_tuser    => axi_user
+        );
+    
+    -- Outputs of the Module
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    fifo_rd_out             <= fifo_rd;
+    fifo_wr_out             <= fifo_trig;
+    filt_out                <= data_aux;
+    trigger                 <= trigger_aux;                     --write;
+    xcorr_data_out          <= xcorr_data_out_aux;
+    fifo_a_empty            <= afe_fifo_a_empty;
+    fifo_empty              <= afe_fifo_empty;
+    fifo_a_full             <= afe_fifo_a_full;
+    fifo_full               <= afe_fifo_full;
+    fifo_wr_err             <= afe_fifo_wr_err;
+    fifo_rd_err             <= afe_fifo_rd_err;
+
+end Behavioral;
