d084b3959e6e5fb132dfbb24768717a1a23e3a15
DanAvi17 <daniel.avila@eia.edu.co>
2023-11-30 15:22:38 -0500

    Generated Bitstream for manual allignment
    The bitstream that allows for manual allignment for the data outputs of the AFE5808A has been succesfully generated, a warning was also issued by showing that the Slave Iserdese for the frame clock has been removed, this warning can be ignored since this is normal, bcause the frame clock is actually unused in this design mode. Must test it

--- daphne_v1/daphne_v1.runs/impl_1/daphne1.tcl
+++ daphne_v1/daphne_v1.runs/impl_1/daphne1.tcl
@@ -115,9 +115,6 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
-set_msg_config -id {Common 17-41} -limit 10000000
-set_msg_config -id {HDL 9-1061} -limit 100000
-set_msg_config -id {HDL 9-1654} -limit 100000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Init Design" START { ROLLUP_AUTO }
@@ -126,7 +123,6 @@ set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
   set_param chipscope.maxJobs 3
-  set_param xicom.use_bs_reader 1
   set_param runs.launchOptions { -jobs 12  }
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a200tfbg676-2
